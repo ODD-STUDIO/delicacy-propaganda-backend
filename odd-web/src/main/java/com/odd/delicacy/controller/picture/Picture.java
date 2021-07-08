45e315a1cf1df83b3c080923bfa525748ac118df
Tanglinfeng <352696800@qq.com>
2021-07-08 18:27:31 +0800

    feat: add Picture 类

---
+++ odd-web/src/main/java/com/odd/delicacy/controller/picture/Picture.java
@@ -0,0 +1,32 @@
+package com.odd.delicacy.controller.picture;
+
+import com.odd.delicacy.base.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 存放所有图片
+ *
+ * @author Tanglinfeng
+ * @date 2021/7/8 18:22
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Picture extends BaseEntity<Picture> {
+
+    private String url;
+
+    /** 关联 id */
+    private Long associateId;
+
+    /** 图片状态：0表示正常，1表示屏蔽 */
+    private Integer status;
+
+    /** 图片排序，大的在前小的在后，默认为 0 */
+    private Integer orderNum;
+
+    /** 图片类型：GOOD、NEWS 等 */
+    private String type;
+}
