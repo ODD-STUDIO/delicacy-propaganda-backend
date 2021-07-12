1a769f3ff76433c3c3bd698ad1f8d5d5c7a779e7
leojangh <lz@genlz.com>
2021-07-12 14:00:08 +0800

    Add convenient constants.

---
+++ commons/src/main/java/com/odd/delicacy/util/Constants.java
@@ -0,0 +1,12 @@
+package com.odd.delicacy.util;
+
+public class Constants {
+
+    public static final String PICTURE_TYPE_GOODS = "GOODS";
+    public static final String PICTURE_TYPE_NEWS = "NEWS";
+    public static final String PICTURE_BASE_URL = "http://img.afblog.love/";
+
+    public static final int PICTURE_STATUS_NORMAL = 0;
+    public static final int PICTURE_STATUS_DISABLE = 1;
+    public static final int PICTURE_STATUS_SHOW_IN_HOME = 2;
+}
